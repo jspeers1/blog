2ddd2b16a95bd9e80665c8f2500c179673045d6a
Jayden Speers <jspeers1@ivytech.edu>
2025-12-01 22:48:45 -0600

    Added PythonAnywhere domain to ALLOWED_HOSTS

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-soxa*@tlnfp08v86=2_5q7*5n_of#twe*3j-7jamd^t*4%c^3%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jspeers1.pythonanywhere.com']
+
 
 
 # Application definition
